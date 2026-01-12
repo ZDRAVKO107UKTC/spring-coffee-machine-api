4e00be0e38960f744a18468844eb6c5b3f673046
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2026-01-12 17:48:39 +0200

    Initial commit

---
+++ src/main/java/com/school/coffeemachine/CoffeeMachineApplication.java
@@ -0,0 +1,13 @@
+package com.school.coffeemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMachineApplication.class, args);
+	}
+
+}
